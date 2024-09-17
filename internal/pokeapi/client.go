c6f0a2e53e4126183ec9b01219e0a93dabb497f8
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-17 15:51:58 -0300

    edit config files

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,14 +3,17 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+	"github.com/Santiparra/Pokedex-CLI/internal/pokecache"
 )
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, interval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(interval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
