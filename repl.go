eba6a9b3dc1d34a280c2631f3a536a5de2dc1343
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-17 13:23:21 -0300

    basic cli commands

---
+++ repl.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func runRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("PokeDex >")
+		reader.Scan()
+
+		inputs := cleanInput(reader.Text())
+		if len(inputs) == 0 {
+			continue
+		}
+
+		commandName := inputs[0]
+
+		command, exist := getCommands()[commandName]
+		if exist {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Command not Found")
+			continue
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Display help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit CLI",
+			callback: commandExit,
+		},
+	}
+}
